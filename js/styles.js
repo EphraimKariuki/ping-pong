7cf08ace6a7298f21b114735ea967b6a1b544b21
Ephraim Kariuki <ephraimkariuki1@gmail.com>
2018-07-13 17:23:08 +0300

    User interface logic

--- js/styles.js
+++ js/styles.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
-  // $(#btnPingPong).submit(function(){
-    // var input = parseInt($(input#inputNumber).va());
-   // business logic-
-    var array = [];
-    var result = "";
-    for (var index = 1; index <=input; index+=1){
-      if ((index%3===0)&&(index%5===0)){
-       result= "ping-pong"
-      }else if (index%5 ===0){
-        result= "pong"
-      }else if (index%3 === 0) {
-         result= "ping"
-      } else{
-        result= index
-      }
-      array.push(result);
-    }
-    alert(array);
-    //user interface logic
-    
+  $("form").submit(function(){
+    // business logic-
+     var input = $("#inputNumber").val();
+     var array = [];
+     var result = "";
+     for (var index = 1; index <=input; index+=1){
+           if ((index%3===0)&&(index%5===0)){
+            result= "ping-pong"
+           }else if (index%5 ===0){
+             result= "pong"
+           }else if (index%3 === 0) {
+              result= "ping"
+           } else{
+             result= index
+           }
+           array.push(result);
+         }
+         alert(array);
+     //user interface logic
+     array.forEach(function (result){
+          $(".result").html("li");
+          $(".result :last-child").text(result);
+     });
+  });
 
 });
