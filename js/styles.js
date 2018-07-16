c141dd168044c709b1ff299b036fd0b6552d47b3
Ephraim Kariuki <ephraimkariuki1@gmail.com>
2018-07-16 15:56:24 +0300

    seperent of business logic and user interface logic

--- js/styles.js
+++ js/styles.js
@@ -1,27 +1,31 @@
+
+  // business logic-
+  // var input = $("#inputNumber").val();
+  var array = [];
+  var result = "";
+  function pingpong (answer){
+    for (var index = 1; index<=answer; index+=1){
+          if ((index%3===0)&&(index%5===0)){
+           result= "ping-pong"
+          }else if (index%5 ===0){
+            result= "pong"
+          }else if (index%3 === 0) {
+             result= "ping"
+          } else{
+            result= index
+          }
+          array.push(result);
+        }
+  }
+
+//user interface logic
 $(document).ready(function(){
-  $("form").submit(function(){
-    // business logic-
-     var input = $("#inputNumber").val();
-     var array = [];
-     var result = "";
-     for (var index = 1; index <=input; index+=1){
-           if ((index%3===0)&&(index%5===0)){
-            result= "ping-pong"
-           }else if (index%5 ===0){
-             result= "pong"
-           }else if (index%3 === 0) {
-              result= "ping"
-           } else{
-             result= index
-           }
-           array.push(result);
-         }
-         alert(array);
-     //user interface logic
-     array.forEach(function (result){
-          $(".result").html("li");
-          $(".result :last-child").text(result);
-     });
+  $("#button").click(function(){
+    var answer = $("#inputNumber").val();
+    pingpong(answer);
+    array.forEach(function(answer){
+      $("#result").append("<li>"+answer + "</li>");
+    });
   });
 
 });
